2250217a22b077f4c42b4f61ae665019d2728dc7
Gilbert Lavergne-Shank <contact@gilbertls.com>
2016-12-07 23:20:15 -0500

    Fixed infinite loading when no poster is available on Android.

--- app/android/components/moviePoster/index.js
+++ app/android/components/moviePoster/index.js
@@ -7,6 +7,7 @@ import {
   View,
   TouchableOpacity,
   Image,
+  Text,
   TouchableNativeFeedback,
 } from 'react-native';
 
@@ -38,14 +39,30 @@ export default class MoviePoster extends React.Component {
         onPress={this.handleOnPress}
         delayPressIn={0}>
         <View style={styles.poster}>
-            <FitImage
-              originalWidth={342}
-              originalHeight={513}
-              indicator
-              indicatorColor='#ffc107' // react native colors or color codes like #919191
-              indicatorSize='large'
-              source={{ uri: 'http://image.tmdb.org/t/p/w342' + this.props.movie.poster_path}}
-            />
+            {
+              !!this.props.movie.poster_path &&
+              <FitImage
+                originalWidth={342}
+                originalHeight={513}
+                indicator
+                indicatorColor='#ffc107' // react native colors or color codes like #919191
+                indicatorSize='large'
+                source={{ uri: 'http://image.tmdb.org/t/p/w342' + this.props.movie.poster_path}}
+              />
+            }
+            {
+              !this.props.movie.poster_path &&
+              <FitImage
+                originalWidth={342}
+                originalHeight={513}
+                indicator
+                indicatorColor='#ffc107' // react native colors or color codes like #919191
+                indicatorSize='large'
+                source={{ uri: 'http://i.imgur.com/OgivBtC.png'}}
+              >
+                <Text style={styles.title}>{this.props.movie.title}</Text>
+              </FitImage>
+            }
         </View>
       </TouchableNativeFeedback>
     );
@@ -62,4 +79,13 @@ const styles = StyleSheet.create({
     paddingLeft: 3,
     paddingTop: 3,
   },
+  title: {
+    color: 'white',
+    bottom: 0,
+    left: 0,
+    fontSize: 14,
+    zIndex: 1000,
+    position: 'absolute',
+    margin: 4,
+  }
 });
